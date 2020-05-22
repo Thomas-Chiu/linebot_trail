571b05452d6ef98b0712c805c21ac599ea7d6cdc
Thomas-Chiu <josemich0511@gmail.com>
2020-05-22 16:40:23 +0800

    0522

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import linebot from 'linebot'
 import dotenv from 'dotenv'
 import rp from 'request-promise'
+// ngrok authtoken 1b9lrgl1tsqi7SVmgWFH1CIjONc_7W9qky1kaudPv1r36PdBG
+// ngrok http 8080
 
 // 啟用dotenv 套件
 dotenv.config()
@@ -42,23 +44,133 @@ bot.on('message', async (event) => {
     // !!! 注意 !!! 使用者查詢要輸入「臺」北、中、南、東，不能用「台」
     const filterData = filter(event.message.text, data)
     if (filterData.length > 0) {
-      msg = `
-    ${event.message.text}有${filterData.length}條路線\n
-    名稱：${filterData[0].TR_CNAME}\n
-    位置：${filterData[0].TR_POSITION}\n
-    簡介：${filterData[0].GUIDE_CONTENT}\n
-    網址：${filterData[0].URL}\n
-    入口：${filterData[0].TR_ENTRANCE[0].memo}\n
-    全長：${filterData[0].TR_LENGTH}\n
-    海拔：${filterData[0].TR_ALT_LOW}～${filterData[0].TR_ALT}\n
-    路程規劃：${filterData[0].TR_TOUR}\n
-    管理單位：${filterData[0].TR_ADMIN}\n
-    洽詢電話：${filterData[0].TR_ADMIN_PHONE}\n
-    最佳造訪期：${filterData[0].TR_BEST_SEASON}\n
-    `
+      for (const f in filterData) { }
+      // msg = {
+      //   type: 'template',
+      //   altText: 'sorry 只能在手機上看到喔',
+      //   template: {
+      //     type: 'carousel',
+      // columns: [
+      //   {
+      //     text: `${filterData[0].TR_CNAME}`,
+      //     actions: [
+      //       {
+      //         type: 'message',
+      //         label: '第一個按鈕',
+      //         text: '1'
+      //       }
+      //     ]
+      //   },
+      //   {
+      //     text: `${filterData[1].TR_CNAME}`,
+      //     actions: [
+      //       {
+      //         type: 'message',
+      //         label: '第一個按鈕',
+      //         text: '1'
+      //       }
+      //     ]
+      //   },
+      //   {
+      //     text: `${filterData[2].TR_CNAME}`,
+      //     actions: [
+      //       {
+      //         type: 'message',
+      //         label: '第一個按鈕',
+      //         text: '1'
+      //       }
+      //     ]
+      //   },
+      //   {
+      //     text: `${filterData[3].TR_CNAME}`,
+      //     actions: [
+      //       {
+      //         type: 'message',
+      //         label: '第一個按鈕',
+      //         text: '1'
+      //       }
+      //     ]
+      //   },
+      //   {
+      //     text: `${filterData[4].TR_CNAME}`,
+      //     actions: [
+      //       {
+      //         type: 'message',
+      //         label: '第一個按鈕',
+      //         text: '1'
+      //       }
+      //     ]
+      //   },
+      //   {
+      //     text: `${filterData[5].TR_CNAME}`,
+      //     actions: [
+      //       {
+      //         type: 'message',
+      //         label: '第一個按鈕',
+      //         text: '1'
+      //       }
+      //     ]
+      //   },
+      //   {
+      //     text: `${filterData[6].TR_CNAME}`,
+      //     actions: [
+      //       {
+      //         type: 'message',
+      //         label: '第一個按鈕',
+      //         text: '1'
+      //       }
+      //     ]
+      //   },
+      //   {
+      //     text: `${filterData[7].TR_CNAME}`,
+      //     actions: [
+      //       {
+      //         type: 'message',
+      //         label: '第一個按鈕',
+      //         text: '1'
+      //       }
+      //     ]
+      //   },
+      //   {
+      //     text: `${filterData[8].TR_CNAME}`,
+      //     actions: [
+      //       {
+      //         type: 'message',
+      //         label: '第一個按鈕',
+      //         text: '1'
+      //       }
+      //     ]
+      //   },
+      //   {
+      //     text: `${filterData[9].TR_CNAME}`,
+      //     actions: [
+      //       {
+      //         type: 'message',
+      //         label: '第一個按鈕',
+      //         text: '1'
+      //       }
+      //     ]
+      //   }
+      // ]
+      // }
+
+      //   msg = `
+      // ${event.message.text}有${filterData.length}條路線\n
+      // 名稱：${filterData[0].TR_CNAME}\n
+      // 位置：${filterData[0].TR_POSITION}\n
+      // 簡介：${filterData[0].GUIDE_CONTENT}\n
+      // 網址：${filterData[0].URL}\n
+      // 入口：${filterData[0].TR_ENTRANCE[0].memo}\n
+      // 全長：${filterData[0].TR_LENGTH}\n
+      // 海拔：${filterData[0].TR_ALT_LOW}～${filterData[0].TR_ALT}\n
+      // 路程規劃：${filterData[0].TR_TOUR}\n
+      // 管理單位：${filterData[0].TR_ADMIN}\n
+      // 洽詢電話：${filterData[0].TR_ADMIN_PHONE}\n
+      // 最佳造訪期：${filterData[0].TR_BEST_SEASON}\n
+      // `
       console.log('成功抓到!')
     }
-    if (event.message.text === 'hi') {
+    if (event.message.text === filterData + 'hi') {
       msg = {
         type: 'location',
         title: '南澳古道',
@@ -67,29 +179,8 @@ bot.on('message', async (event) => {
         longitude: 121.691964454609
       }
     }
-    if (event.message.text === 'hey') {
-      msg = {
-        type: 'template',
-        altText: '在不支援顯示樣板的地方顯示的文字',
-        template: {
-          // confirm 只能使用兩個action
-          type: 'confirm',
-          text: '標題文字',
-          actions: [
-            {
-              type: 'message',
-              label: '第一個按鈕',
-              text: '1'
-            },
-            {
-              type: 'message',
-              label: '第二個按鈕',
-              text: '2'
-            }
-          ]
-        }
-      }
-    }
+
+    // carouesl 模板，最多可以放10 個column，column 的 actions 的數量必須相同，label (按鈕) 最多只能放 3 個
     if (event.message.text === 'ha') {
       msg = {
         type: 'template',
@@ -116,11 +207,92 @@ bot.on('message', async (event) => {
                   text: '1'
                 }
               ]
+            },
+            {
+              text: '第一組標題',
+              actions: [
+                {
+                  type: 'message',
+                  label: '第一個按鈕',
+                  text: '1'
+                }
+              ]
+            },
+            {
+              text: '第一組標題',
+              actions: [
+                {
+                  type: 'message',
+                  label: '第一個按鈕',
+                  text: '1'
+                }
+              ]
+            },
+            {
+              text: '第一組標題',
+              actions: [
+                {
+                  type: 'message',
+                  label: '第一個按鈕',
+                  text: '1'
+                }
+              ]
+            },
+            {
+              text: '第一組標題',
+              actions: [
+                {
+                  type: 'message',
+                  label: '第一個按鈕',
+                  text: '1'
+                }
+              ]
+            },
+            {
+              text: '第一組標題',
+              actions: [
+                {
+                  type: 'message',
+                  label: '第一個按鈕',
+                  text: '1'
+                }
+              ]
+            },
+            {
+              text: '第一組標題',
+              actions: [
+                {
+                  type: 'message',
+                  label: '第一個按鈕',
+                  text: '1'
+                }
+              ]
+            },
+            {
+              text: '第一組標題',
+              actions: [
+                {
+                  type: 'message',
+                  label: '第一個按鈕',
+                  text: '1'
+                }
+              ]
+            },
+            {
+              text: '第一組標題',
+              actions: [
+                {
+                  type: 'message',
+                  label: '第一個按鈕',
+                  text: '1'
+                }
+              ]
             }
           ]
         }
       }
     }
+    // buttons 模板，最多可放四個label (按鈕)，不過是直式的
     if (event.message.text === 'he') {
       msg = {
         type: 'template',
